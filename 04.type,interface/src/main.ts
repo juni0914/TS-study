744e56668d67ac232c52ea2e498911eaf80f2bac
juni0914 <blueblue870@naver.com>
2023-11-17 18:02:57 +0900

    2023-11-17

---
+++ 04.type,interface/src/main.ts
@@ -0,0 +1,101 @@
+// interface Animal {
+//     name: string;
+// }
+
+// interface Bear extends Animal {
+//     honey: boolean;
+// }
+
+// const bear1: Bear = {
+//     name: 'honey bear',
+//     honey: true
+// }
+
+
+
+// type Animal = {
+//     name: string;
+// }
+
+// type Bear = Animal & {
+//     honey: boolean;
+// }
+
+// const bear1: Bear = {
+//     name: 'honey bear',
+//     honey: true
+// }
+
+
+
+// interface Animal {
+//     name: string;
+// }
+
+// interface Animal {
+//     honey: boolean;
+// }
+
+
+// const bear1: Animal = {
+//     name: 'honey bear',
+//     honey: true
+// }
+
+
+
+// type Animal = {
+//     name: string;
+// }
+
+// type Animal = {
+//     honey: boolean;
+// }
+
+
+
+
+// interface Animal {
+//     name: string;
+// }
+
+// interface Animal {
+//     honey: boolean;
+// }
+
+
+
+// interface Animal {
+//     name: string;
+// }
+
+
+// interface Bear {
+//     honey: boolean;
+// }
+
+// type NewType = Animal | Bear;
+
+// const bear1: NewType = {
+//     name: 'honey bear',
+//     honey: true
+// }
+
+
+
+// type Animal = {
+//     name: string;
+// }
+
+
+// type Bear = {
+//     honey: boolean;
+// }
+
+// type NewType = Animal | Bear;
+
+// const bear1: NewType = {
+//     name: 'honey bear',
+//     honey: true
+// }
+
