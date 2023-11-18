2605bcb212e6a3bc6f5d0d611a898a24e9876374
juni0914 <blueblue870@naver.com>
2023-11-18 18:06:07 +0900

    2023-11-18

---
+++ 05.call-signature,index-signature/src/main.ts
@@ -0,0 +1,42 @@
+// interface getLikeNumber {       //호출 시그니처
+//     (like: number): number;
+// }
+
+// interface Post {
+//     id: number;
+//     title: string;
+//     getLikeNumber: getLikeNumber
+// }
+
+// const post1: Post = {
+//     id: 1,
+//     title: 'post 1',
+//     getLikeNumber(like: number){
+//         return like
+//     }
+// }
+
+// post1.getLikeNumber(1);
+
+
+
+interface Post {
+    [key: string]: unknown;
+    id: number;
+    title: string;
+}
+
+const post1: Post = {
+    id: 1,
+    title: 'post 1',
+}
+
+post1['description'] = 'description 1';
+post1['pages'] = 300;
+
+
+interface Names {
+    [item: number]: string;
+}
+
+const userNames: Names = ['Lee', 'Kim']
